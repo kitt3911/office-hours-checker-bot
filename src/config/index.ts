1f1981f2e311ab824c4ded47cdaa67efe639cdef
kitt3911 <ivanignatenko28@gmail.com>
2021-03-20 18:08:20 +0600

    init

---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+require('dotenv').config()
+
+export const botToken:string = process.env.BOT_TOKEN || ""
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import { PrismaClient } from '@prisma/client'
+import {Telegraf} from 'telegraf'
+import { botToken } from './config'
+
 
 const prisma = new PrismaClient()
+
+async function main() {
+    const bot = new Telegraf(botToken)
+    bot.start((ctx) => ctx.reply('Welcome'))
+    bot.launch()
+}
+
+main().catch(e => {
+    throw e
+}).finally(async ()=> {
+    await prisma.$disconnect()
+})
